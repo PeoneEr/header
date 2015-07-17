a3e3a19e3566fe5c2b4c0042be7166c479f1c51c
PeoneEr <peoneerko@yandex.ru>
2015-07-17 12:22:20 +0600

    add generator with errors

---
+++ lib/generators/header/pages_generator.rb
@@ -0,0 +1,17 @@
+module Header
+  module Generators
+    class PagesGenerator < Rails::Generators::Base
+
+      desc 'Generate 403 404 500 and 502 error pages'
+      source_root File.expand_path("../templates", __FILE__)
+
+      def copy_error_pages
+        copy_file "403.html", "public/403.html"
+        copy_file "404.html", "public/404.html"
+        copy_file "500.html", "public/500.html"
+        copy_file "502.html", "public/502.html"
+      end
+
+    end
+  end
+end
